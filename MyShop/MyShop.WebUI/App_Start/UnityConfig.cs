51cff929d704631e2b5c749c7ffa6b8e01d8c9de
Greg Vincent <greg.vincent@definition6.com>
2018-01-23 16:50:37 -0500

    Basket UI

--- MyShop/MyShop.WebUI/App_Start/UnityConfig.cs
+++ MyShop/MyShop.WebUI/App_Start/UnityConfig.cs
@@ -48,6 +48,9 @@ namespace MyShop.WebUI
             // container.RegisterType<IProductRepository, ProductRepository>();
             container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
             container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Basket>, SQLRepository<Basket>>();
+            container.RegisterType<IRepository<BasketItem>, SQLRepository<BasketItem>>();
+            container.RegisterType<IBasketService, IBasketService>();
         }
     }
 }
